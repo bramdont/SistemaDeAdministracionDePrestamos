cb2802eb01e17a435d070703b136090455af10b4
Bramdont Garcia <bramdont@gmail.com>
2018-03-22 16:38:20 -0400

    PrestamosController: setting the details view to also see receipts details in that view

--- SistemaDeAdministracionDePrestamos/Models/Recibo.cs
+++ SistemaDeAdministracionDePrestamos/Models/Recibo.cs
@@ -1,6 +1,4 @@
-﻿using SistemaDeAdministracionDePrestamos.Models.ViewModels;
-using System;
-using System.Collections.Generic;
+﻿using System;
 using System.ComponentModel.DataAnnotations;
 
 namespace SistemaDeAdministracionDePrestamos.Models
